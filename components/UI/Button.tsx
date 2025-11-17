0814bba1c628531ac3e9be466a745975cc9828d5
Johan Barrantes <seb_0506@hotmail.com>
2025-11-17 10:33:37 -0500

    Some Types

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,14 +1,19 @@
-// components/ui/Button.tsx
-export default function Button({ children, ...props }) {
+// components/UI/Button.tsx
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export default function Button({ children, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className="
-        w-full py-2 rounded-lg 
-        bg-blue-600 text-white font-semibold
-        hover:bg-blue-700 transition
-        disabled:bg-blue-900 disabled:cursor-not-allowed
-      "
+      className={`
+        w-full bg-blue-600 text-white py-2 rounded-lg 
+        hover:bg-blue-700 transition 
+        ${props.className ?? ""}
+      `}
     >
       {children}
     </button>
